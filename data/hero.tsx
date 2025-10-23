eacdb2bb1e6ef8b6311259b81a1dbbb386b93c2b
touraye <touraye07@gmail.com>
2025-10-23 15:38:58 +0000

    add data - hero

---
+++ data/hero.tsx
@@ -0,0 +1,14 @@
+export const carouselSlides = [
+  { src: "/assets/images/hero-image-1.png", alt: "Doctor smiling with patient" },
+  { src: "/assets/images/hero-image-2.png", alt: "Doctor consulting with a family" },
+  { src: "/assets/images/hero-image-3.png", alt: "Doctor reviewing notes with a patient" },
+  // Add a 4th slide here to test the "scrolling on desktop" logic
+];
+
+export const trustedLogos = [
+  { src: "/assets/icons/airbnb.png", alt: "Airbnb" },
+  { src: "/assets/icons/framer.png", alt: "Framer" },
+  { src: "/assets/icons/amazon.png", alt: "Amazon" },
+  { src: "/assets/icons/microsoft.png", alt: "Microsoft" },
+  { src: "/assets/icons/slack.png", alt: "Slack" },
+];
