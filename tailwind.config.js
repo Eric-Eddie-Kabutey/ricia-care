039329a834bc1d5244c46ca2145779ef58a88378
touraye <touraye07@gmail.com>
2025-10-23 15:38:24 +0000

    setup tailwind config

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+// tailwind.config.js
+module.exports = {
+	theme: {
+		extend: {
+			fontFamily: {
+				sans: ['var(--font-bricolage)', 'system-ui'], // Use as default sans font
+			},
+		},
+	},
+}
