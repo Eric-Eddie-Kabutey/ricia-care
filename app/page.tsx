d7a37947af617290a6b9bfdffc0a11dd95ad780b
touraye <touraye07@gmail.com>
2025-10-23 16:36:09 +0000

    update: add modules

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,24 @@
+import Cta from "@/components/home/cta";
+import Faq from "@/components/home/faq";
+import Hero from "@/components/home/hero";
+import MobileAppCta from "@/components/home/mobile-app-cta";
+import Testimonials from "@/components/home/testimonial";
+
 export default function Home() {
   return (<>
     {/* Hero section */}
+    <Hero />
+
+    {/* Mobile App cta */}
+    <MobileAppCta />    
+
+    {/* Testimonial section */}
+    <Testimonials />
+
+    {/* FAQ section */}
+    <Faq />
+
+    {/* CTA section */}
+    <Cta />
   </>);
 }
